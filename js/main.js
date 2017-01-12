bd394522b8d066244dfeb3f8c23af0bf7cb27c9c
Raymond Perminov <raymondproduction@ukr.net>
2017-01-13 00:26:39 +0200

    Use RequireJS

--- js/main.js
+++ js/main.js
@@ -1,35 +1,40 @@
-var x,y,canvas,rectangle,ctx;
+require(['jquery'], function(){
+ 
+	var x,y,canvas,rectangle,ctx;
 
 
-$(document).ready(function () {
- canvas = document.getElementById('space');
- x=canvas.width/2-10;
- y=canvas.height/2-10;
- if (canvas.getContext){
-        ctx = canvas.getContext('2d');
-        rectangle = new Path2D();
-        rectangle.rect(x, y, 20, 20);
-        ctx.fill(rectangle);
-      }
+	$(document).ready(function () {
+	 canvas = document.getElementById('space');
+	 x=canvas.width/2-10;
+	 y=canvas.height/2-10;
+	 if (canvas.getContext){
+	        ctx = canvas.getContext('2d');
+	        rectangle = new Path2D();
+	        rectangle.rect(x, y, 20, 20);
+	        ctx.fill(rectangle);
+	      }
 
-});
+	});
 
-$(document).keydown(function( event ) {
-        canvas = document.getElementById('space');
-        ctx = canvas.getContext('2d');
-        rectangle = new Path2D();
+	$(document).keydown(function( event ) {
+	        canvas = document.getElementById('space');
+	        ctx = canvas.getContext('2d');
+	        rectangle = new Path2D();
 
-        console.log(event.keyCode);
+	        console.log(event.keyCode);
        
-        if (canvas.getContext){
-         ctx.clearRect(0, 0, canvas.width, canvas.height);
-           if (event.keyCode==37 && x>0) x-=1;
-           if (event.keyCode==38 && y>0) y-=1;
-           if (event.keyCode==39 && x<canvas.width-20) x+=1
-           if (event.keyCode==40 && y<canvas.height-20) y+=1;
-         //rectangle.moveTo(x,y);
-         rectangle.rect(x, y, 20, 20);
-         ctx.fill(rectangle);
-      }
+	        if (canvas.getContext){
+	         ctx.clearRect(0, 0, canvas.width, canvas.height);
+	           if (event.keyCode==37 && x>0) x-=1;
+	           if (event.keyCode==38 && y>0) y-=1;
+	           if (event.keyCode==39 && x<canvas.width-20) x+=1
+	           if (event.keyCode==40 && y<canvas.height-20) y+=1;
+	         //rectangle.moveTo(x,y);
+	         rectangle.rect(x, y, 20, 20);
+	         ctx.fill(rectangle);
+	      }
+
+	});
+
 
 });
