caaa596e3f414cda7bf4fdfcae888c5c56f47bb0
Raymond Perminov <raymondproduction@ukr.net>
2017-01-13 20:57:29 +0200

    Added window.onload = showSquare();

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
 require(['jquery','jcanvas'], function(){
 
-	var x,y,canvas,rectangle,height,width;
+  var x,y,canvas,rectangle,height,width;
 
+  window.onload = showSquare();
+  //$(window).load(showSquare());
+  //$(document).ready(showSquare());
 
-	$(window).ready(function () {
-   height=$('#space').height();
-   width=$('#space').width();
-	 x=width/2;
-	 y=height/2;
-		$('#space').drawRect({
-		  fillStyle: '#000',
-		  x: x, y: y,
-		  width: 20,
-		  height: 20
-	  });
-	});
-	$(window).keydown(function( event ){
-       $('#space').clearCanvas();
+  function showSquare(){
+    canvas = $('#space');
+    height=canvas.height();
+    width=canvas.width();
+    console.log("Hi");
+    x=width/2;
+    y=height/2;
+    canvas.drawRect({
+        fillStyle: '#000',
+        x: x, y: y,
+        width: 20,
+        height: 20
+      });
+  };
+
+	$(document).keydown(function( event ){
+     // console.log("Hi");
+       canvas.clearCanvas();
        if (event.keyCode==37 && x>10) x-=1;
        if (event.keyCode==38 && y>10) y-=1;
        if (event.keyCode==39 && x<width-10) x+=1
        if (event.keyCode==40 && y<height-10) y+=1;
-      $('#space').drawRect({
+      canvas.drawRect({
 			  fillStyle: '#000',
 			  x: x, y: y,
 			  width: 20,
