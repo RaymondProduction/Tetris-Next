21793f18a31b23f4ea70a0a6d6e63ed60c438b79
Raymond Perminov <raymondproduction@ukr.net>
2017-01-13 13:22:28 +0200

    Modification main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
 require(['jquery','jcanvas'], function(){
 
-	var x,y,canvas,rectangle,ctx;
+	var x,y,canvas,rectangle,height,width;
 
 
 	$(document).ready(function () {
-	 canvas = document.getElementById('space');
-	 x=canvas.width/2-10;
-	 y=canvas.height/2-10;
+   height=$('#space').height();
+   width=$('#space').width();
+	 x=width/2;
+	 y=height/2;
 		$('#space').drawRect({
 		  fillStyle: '#000',
 		  x: x, y: y,
 		  width: 20,
 		  height: 20
 	  });
-
 	});
-
-	$(document).keydown(function( event ) {
-       console.log(event.keyCode);
+	$(document).keydown(function( event ){
        $('#space').clearCanvas();
-       if (event.keyCode==37 && x>0) x-=1;
-       if (event.keyCode==38 && y>0) y-=1;
-       if (event.keyCode==39 && x<canvas.width-20) x+=1
-       if (event.keyCode==40 && y<canvas.height-20) y+=1;
+       if (event.keyCode==37 && x>10) x-=1;
+       if (event.keyCode==38 && y>10) y-=1;
+       if (event.keyCode==39 && x<width-10) x+=1
+       if (event.keyCode==40 && y<height-10) y+=1;
       $('#space').drawRect({
 			  fillStyle: '#000',
 			  x: x, y: y,
