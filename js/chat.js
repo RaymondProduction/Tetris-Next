881af57b0ef4db7e45d1f97a56e7a7d3ac5f3a24
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-02-10 19:18:40 +0200

    I removed jQuery in the chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,39 +1,63 @@
 define('chat', [
-    'jquery',
-    'socketio',
+    'socketio'
   ],
-  function(jQ, io) {
+  function(io) {
 
     // Module for organization chat
     function chatObj() {
-
+      this.input =  document.getElementById('m');
+      // имя собеседника
       this.name = '';
+      // подключаем сокет
       this.socket = io();
+      // элемент со списком сообщений
+      this.messages = document.getElementById('messages');
+      // элемент списка, сообщение
+      this.message = document.createElement('li');
+
       // this.socket.io.connect('http://localhost');
     }
 
+    chatObj.prototype.askName = function() {
+      // текст сообщения
+      var text = document.createTextNode('Robot> What is your  name?');
+      // добавить текст в элемент списка
+      this.message.appendChild(text);
+      // добавить элемент списка в список сообщений
+      this.messages.appendChild(this.message);
+    }
+    chatObj.prototype.addMassage = function(msg) {
+      // создаем текст сообщения
+      var text = document.createTextNode(msg);
+      // создаем сообщение, елемент списка
+      message = document.createElement('li');
+      // добавляем текст в сообщение
+      message.appendChild(text);
+      // вставляем сообщение перед предыдущим this.message
+      messages.insertBefore(message, this.message);
+      // теперь это сообщение будет предыдущим
+      this.message = message;
+    }
     chatObj.prototype.start = function() {
       var self = this;
-
-      jQ('#messages').append(jQ('<li>').text('What is your name?'));
-
-
-      jQ('form').submit(function() {
+      this.askName();
+      window.captureEvents(Event.SUBMIT);
+      window.onsubmit = function() {
         if (self.name == '') {
-          self.name = jQ('#m').val();
-          (jQ('<li>').text('Ок. Your name in chat ' + self.name)).prependTo(jQ('#messages'));
-          self.socket.emit('chat message', self.name + ' joined the chat ');
-          jQ('#m').val('');
+          self.name = self.input.value;
+          self.addMassage('Robot> Oк. Your name in chat ' + self.name);
+          self.socket.emit('chat message','Robot> '+ self.name + ' joined the chat ');
+          self.input.value = '';
         } else {
-          self.socket.emit('chat message', self.name + '> ' + jQ('#m').val());
-          jQ('#m').val('');
+          self.socket.emit('chat message', self.name + '> ' + self.input.value);
+          self.input.value = '';
         }
         return false;
-      });
+      };
 
       this.socket.on('chat message', function(msg) {
         if (this.name != '') {
-          (jQ('<li>').text(msg)).prependTo(jQ('#messages'));
+          self.addMassage(msg);
         };
       });
 
