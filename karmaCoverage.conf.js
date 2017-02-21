51061cc9e5b5c447355caabd647ce3b3b4abda60
Raymond Perminov <raymondproduction@ukr.net>
2017-02-21 14:25:21 +0200

    Added coverage 2

--- karmaCoverage.conf.js
+++ karmaCoverage.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'src/**/*.js': ['coverage']
+      'js/**/*.js': ['coverage']
     },
 
     // optionally, configure the reporter
