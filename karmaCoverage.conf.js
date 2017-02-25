3153f85b9dc39eb778261e621c762ef06d26e4ab
Raymond Perminov <raymondproduction@ukr.net>
2017-02-25 11:30:31 +0200

    Fix karmaCoverage.conf.js

--- karmaCoverage.conf.js
+++ karmaCoverage.conf.js
@@ -38,7 +38,7 @@ module.exports = function(config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'js/**/*.js': ['coverage']
+      './src/js/**/*.js': ['coverage']
     },
 
 
