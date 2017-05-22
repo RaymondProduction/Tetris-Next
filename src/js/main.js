4399494b905b3ef1898154a3e184e4d848acbd9a
Raymond Perminov <raymondproduction@ukr.net>
2017-05-22 20:46:08 +0300

    Get token

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,15 @@ require(['chat', 'cube', 'session'],
     function start() {
 
       console.log(getUrlVars()['t']);
+      var request = new XMLHttpRequest();
+      request.setRequestHeader('authorization', 'token ' + getUrlVars()['t']);
+      request.setRequestHeader('accept', 'application/json');
+      request.open("GET", "https://api.github.com/user", true);
+      request.onload = function() {
+        alert(request.responseText);
+        console.log(request.responseText);
+      }
+      request.send(null);
 
       // активация чат
       chat = new ChatModule();
