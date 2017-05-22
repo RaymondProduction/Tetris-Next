1951b469929a080e3119cab781dbf75e11881094
Raymond Perminov <raymondproduction@ukr.net>
2017-05-22 07:35:15 +0300

    Show login

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,22 @@
 require(['chat', 'cube', 'session'],
   function(ChatModule, CubeModule) {
+    function getUrlVars() {
+      var vars = {};
+      var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value;
+      });
+      return vars;
+    }
 
     document.addEventListener('DOMContentLoaded', start());
 
     function start() {
+
+      console.log(getUrlVars()['login']);
+
       // активация чат
-       chat = new ChatModule();
-       chat.start();
+      chat = new ChatModule();
+      chat.start();
       // создаем куб
       cube = new CubeModule(20);
       cube.start();
