bcc24faf5e79c1552771d9612e195dabfe7fd75e
Raymond Perminov <raymondproduction@ukr.net>
2017-05-23 09:54:44 +0300

    get user data

--- src/js/main.js
+++ src/js/main.js
@@ -12,20 +12,19 @@ require(['chat', 'cube', 'session'],
 
     function start() {
 
-      console.log('Cookie: ',document.cookie);
-
       console.log(getUrlVars()['t']);
       var request = new XMLHttpRequest();
-      request.open("GET", "https://api.github.com/user", true);
-      request.setRequestHeader('authorization', 'token ' + getUrlVars()['t']);
+      request.open("POST", "https://tetris-next.net/test", true);
+   //   request.setRequestHeader('authorization', 'token ' + getUrlVars()['t']);
       request.setRequestHeader('accept', 'application/json');
       request.onload = function() {
         //alert(request.responseText);
         //console.log(request.responseText);
         var res = JSON.parse(request.responseText);
-        console.log('Login:',res.login);
-        console.log('name: ', res.name);
-        console.log('id:', res.id);
+        console.log(res);
+        // console.log('Login:',res.login);
+        // console.log('name: ', res.name);
+        // console.log('id:', res.id);
       }
       request.send(null);
 
