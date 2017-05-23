336cb3303872014a625563a828b27aea5df1e8b7
Raymond Perminov <raymondproduction@ukr.net>
2017-05-23 18:26:54 +0300

    Get token

--- src/js/chat.js
+++ src/js/chat.js
@@ -43,7 +43,7 @@ define('chat', ['session'],
       //var h = document.getElementById('name');
       //h.appendChild(this.name);
 
-      document.getElementById("name")[0].innerHTML = this.name;
+      document.getElementById("name")[0].innerHTML = 'dfdsg';
 
       // обрадуем пользователя что он подключен
       this.addMassage('Robot> Oк. Your name in chat ' + this.name);
