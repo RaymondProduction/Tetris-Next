4a1ee9e497004290123f467193ac7bcbbeb02859
Raymond Perminov <raymondproduction@ukr.net>
2017-05-23 14:00:27 +0300

    Get token

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,15 @@
 require(['chat', 'cube', 'session'],
   function(ChatModule, CubeModule) {
-    function getUrlVars() {
-      var vars = {};
-      var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
-        vars[key] = value;
-      });
-      return vars;
-    }
 
     document.addEventListener('DOMContentLoaded', start());
 
     function start() {
 
-      console.log(getUrlVars()['t']);
       var request = new XMLHttpRequest();
       request.open("POST", "https://tetris-next.net/test", true);
-   //   request.setRequestHeader('authorization', 'token ' + getUrlVars()['t']);
       request.setRequestHeader('accept', 'application/json');
       request.onload = function() {
-        //alert(request.responseText);
-        //console.log(request.responseText);
         var res = JSON.parse(request.responseText);
-        //console.log(request.responseText);
         console.log('Login:',res.login);
         console.log('name: ', res.name);
         console.log('id:', res.id);
